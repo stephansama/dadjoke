7f8d59b0ee7292d3229c22f27dfa8905dfa9b5be
Stephan Randle <stephanrandle@hotmail.com>
2022-04-09 00:22:15 -0400

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+
+export const Navbar = () => {
+	return (
+		<div>
+			<AppBar position='static' color='primary'>
+				<Toolbar>
+					<Typography variant='h6'>Dad Joke Engine</Typography>
+				</Toolbar>
+			</AppBar>
+		</div>
+	)
+}
