67c6d9ca6d6e82384fc0c1ecc3eb7091f09d5484
Stephan Randle <stephanrandle@hotmail.com>
2022-04-10 00:54:37 -0400

    connected to dadjoke api

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,25 +1,67 @@
 import { createTheme, ThemeProvider } from '@mui/material'
 import { createContext, useMemo, useState } from 'react'
 
+import axios from 'axios'
+
+import { useLocalStorage } from '../hooks/useLocalStorage'
+
 export const GlobalContext = createContext()
 
+const axiosConfig = {
+	headers: {
+		'User-Agent': 'https://github.com/stephansama/dadjoke',
+		Accept: 'application/json',
+	},
+}
+
 export const GlobalProvider = ({ children }) => {
-	const [darkmode, setDarkmode] = useState('dark')
+	const [darkmode, setDarkmode] = useLocalStorage('darkmode', 'light')
+	const [jokes, setJokes] = useLocalStorage('jokes', [])
+	const [randomJoke, setRandomJoke] = useState('')
 
 	const toggleDark = () =>
 		setDarkmode((old) => (old === 'light' ? 'dark' : 'light'))
 
+	const findJokeById = (id) => jokes.filter((joke) => joke.id === id)[0].joke
+
+	const searchForJoke = (query) => {
+		axios
+			.get(
+				`${process.env.REACT_APP_API_BASE_URL}search?term=${query}`,
+				axiosConfig
+			)
+			.then((data) => setJokes(data.data.results))
+			.catch((err) => console.error(err))
+	}
+
+	const getRandomJoke = () => {
+		axios
+			.get(`${process.env.REACT_APP_API_BASE_URL}`, axiosConfig)
+			.then((data) => setRandomJoke(data.data.joke))
+			.catch((err) => console.error(err))
+	}
+
+	// Material UI Theme
 	const GlobalTheme = useMemo(
-		() =>
-			createTheme({
-				palette: { mode: darkmode },
-			}),
+		() => createTheme({ palette: { mode: darkmode } }),
 		[darkmode]
 	)
 
 	return (
-		<GlobalContext.Provider value={{ darkmode, toggleDark }}>
-			<ThemeProvider theme={GlobalTheme}>{children}</ThemeProvider>
+		<GlobalContext.Provider
+			value={{
+				darkmode,
+				toggleDark,
+				jokes,
+				randomJoke,
+				findJokeById,
+				searchForJoke,
+				getRandomJoke,
+			}}
+		>
+			<ThemeProvider theme={GlobalTheme}>
+				<>{children}</>
+			</ThemeProvider>
 		</GlobalContext.Provider>
 	)
 }
