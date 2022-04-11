58a28ef604fd6d84fe1c4251ac576c1f776aa2b0
Stephan Randle <stephanrandle@hotmail.com>
2022-04-11 16:52:20 -0400

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,40 @@
+import { Box, Divider, IconButton, Typography } from '@mui/material'
+
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+
 export const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<div>
+			<Divider />
+			<Box
+				sx={{
+					my: '5px',
+					mx: '10px',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Typography variant='body1'>&copy; 2022</Typography>
+				{/* LINKS */}
+				<div>
+					<IconButton
+						component='a'
+						href='https://github.com/stephansama'
+						target='_blank'
+					>
+						<GitHubIcon />
+					</IconButton>
+					<IconButton
+						component='a'
+						href='https://www.linkedin.com/in/stephan-randle-38a30319a/'
+						target='_blank'
+					>
+						<LinkedInIcon />
+					</IconButton>
+				</div>
+			</Box>
+		</div>
+	)
 }
