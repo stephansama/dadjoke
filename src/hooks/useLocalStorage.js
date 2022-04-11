1561c155dc893745cfeab33ec706d18f12e24d2e
Stephan Randle <stephanrandle@hotmail.com>
2022-04-11 16:53:15 -0400

    updated localstorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,13 +4,14 @@ export const useLocalStorage = (key, initialValue) => {
 	const [value, setValue] = useState(() => {
 		const savedValue = JSON.parse(localStorage.getItem(key))
 		if (savedValue) return savedValue
-		if (initialValue instanceof Function) return initialValue()
-		return initialValue
+
+		return initialValue instanceof Function ? initialValue() : initialValue
 	})
 
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value))
-	}, [value, key])
+	useEffect(
+		() => localStorage.setItem(key, JSON.stringify(value)),
+		[value, key]
+	)
 
 	return [value, setValue]
 }
